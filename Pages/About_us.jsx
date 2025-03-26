5e09fb990ace160df14dc1423964df69ebfd1ca2
RangoSS <tshidaisa@outlook.com>
2025-03-26 10:42:47 +0200

    removed header

--- Pages/About_us.jsx
+++ Pages/About_us.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Table from '../src/Components/Table'
-import Header from '../src/Components/header'
 
 
 
@@ -8,7 +7,7 @@ import Header from '../src/Components/header'
 const About_us = () => {
     return (
         <div className='container text-center' style={{ color: 'blue', fontSize: '24px', border: 'solid red' }}>
-            <Header />
+    
             <h1 className=''>about us page</h1>
 
             <Table />
--- Pages/Contact.jsx
+++ Pages/Contact.jsx
@@ -1,11 +1,11 @@
 
 import React from 'react'
-import Header from '../src/Components/header.jsx';
+
 
 const Contact = () => {
     return (
         <div className='container'>
-            <Header />
+            
             <h2>contact</h2>
         </div>
     )
